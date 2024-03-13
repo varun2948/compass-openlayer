7b469267a6990c65d09d1afb1244890dc11ec5f6
Deepak Pradhan <deepak.pradhan048@gmail.com>
2024-03-13 20:01:25 +0545

    fix: new route added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import BasicCompass from "./BasicCompass.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/compass-openlayer/compass" element={<BasicCompass />} />
+      <Route path="/compass-openlayer/" element={<App />} />
+    </>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
